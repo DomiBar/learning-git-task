0e60936bf60d93300c0690622faa1cb78135a169
Dominik Baran <dominik.baran1992@gmail.com>
2020-05-23 16:58:23 +0200

    Add listing loops

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ shopping_list={
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 items=0
+
+print("Lista zakupów")
+for shop in shopping_list:
+    for i in range(len(shopping_list[shop])):
+        shopping_list[shop][i]=shopping_list[shop][i].capitalize()
+    print(f"Idę do {shop.capitalize()} i kupuję tam {shopping_list[shop]}")
+    items+=len(shopping_list[shop])
